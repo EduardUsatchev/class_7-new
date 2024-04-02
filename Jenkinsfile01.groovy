69c0d45c6ae1d3b591c96d850963e3740149b37d
Eduard Usatchev <eduard.usatchev@gmail.com>
2024-04-02 21:27:15 +0300

    Jenkins disk resizer

---
+++ Jenkinsfile01.groovy
@@ -0,0 +1,11 @@
+properties([pipelineTriggers([pollSCM('* * * * *')])])
+node("edi"){
+    stage("clone"){
+        git branch: 'main', url: 'https://github.com/EduardUsatchev/class_7-new.git'
+
+    }
+    stage("execute"){
+        sh "ls -l"
+        sh "/Users/e0u00jg/anaconda3/bin/python3 disk_resizer.py"
+    }
+}
